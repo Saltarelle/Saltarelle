f44c19d3a13e09bff0fa4546e46b687a5ee28af3
erik.kallen@gmail.com <erik.kallen@gmail.com@7e296dba-0ccf-11df-bbbe-ede73d0abfb9>
2010-02-21 00:10:31 +0000

    Created <switch> expression
    git-svn-id: http://saltarelle.googlecode.com/svn/trunk@77 7e296dba-0ccf-11df-bbbe-ede73d0abfb9

--- Saltarelle/Core/SaltarelleParser/SaltarelleParser.Shared/NodeProcessors/ControlFlowTagProcessor.Shared.cs
+++ Saltarelle/Core/SaltarelleParser/SaltarelleParser.Shared/NodeProcessors/ControlFlowTagProcessor.Shared.cs
@@ -34,24 +34,79 @@ namespace Saltarelle.NodeProcessors {
 						throw ParserUtils.TemplateErrorException("The <while> element must have the test attribute specified.");
 					return "while (" + Utils.NodeValue(testAttr) + ")";
 				}
+				case "switch": {
+					XmlAttribute exprAttr = (XmlAttribute)node.Attributes.GetNamedItem("expr");
+					if (exprAttr == null)
+						throw ParserUtils.TemplateErrorException("The <switch> element must have the expr attribute specified.");
+					return "switch (" + Utils.NodeValue(exprAttr) + ")";
+				}
 				default:
 					return null;
 			}
 		}
-	
+		
+		internal static void ProcessSwitchContent(IDocumentProcessor docProcessor, XmlNode switchNode, ITemplate template, IRenderFunction currentRenderFunction) {
+			bool hasDefault = false;
+			bool hasAny = false;
+			Utils.DoForEachChild(switchNode, delegate(XmlNode child) {
+				#if CLIENT
+					if (((child.NodeType == XmlNodeType.Text || child.NodeType == XmlNodeType.CharacterData) && child.NodeValue.Trim() == "") || child.NodeType == XmlNodeType.Comment)
+						return;
+				#else
+					if (child.NodeType == XmlNodeType.Whitespace || child.NodeType == XmlNodeType.SignificantWhitespace || child.NodeType == XmlNodeType.Comment)
+						return;
+				#endif
+
+				if (child.NodeType == XmlNodeType.Element && Utils.NodeName(child) == "case") {
+					XmlAttribute valueAttr = (XmlAttribute)child.Attributes.GetNamedItem("value");
+					if (valueAttr == null)
+						throw ParserUtils.TemplateErrorException("The <case> element must have the value attribute specified.");
+					currentRenderFunction.AddFragment(new CodeFragment("case " + Utils.NodeValue(valueAttr) + ": {", 1));
+				}
+				else if (child.NodeType == XmlNodeType.Element && Utils.NodeName(child) == "default") {
+					if (hasDefault)
+						throw ParserUtils.TemplateErrorException("There can only be one <default> element inside <switch>");
+					hasDefault = true;
+					currentRenderFunction.AddFragment(new CodeFragment("default: {", 1));
+				}
+				else
+					throw ParserUtils.TemplateErrorException("The <switch> element can only have <case> and <default> elements as children.");
+
+				hasAny = true;
+				Utils.DoForEachChild(child, delegate(XmlNode grandchild) {
+					docProcessor.ProcessRecursive(grandchild, template, currentRenderFunction);
+				});
+
+				currentRenderFunction.AddFragment(new CodeFragment("break;", -1));
+				currentRenderFunction.AddFragment(new CodeFragment("}", 0));
+			});
+			if (!hasAny)
+				throw ParserUtils.TemplateErrorException("The <switch> element must contain at least one <case> or <default>");
+		}
+
 		public bool TryProcess(IDocumentProcessor docProcessor, XmlNode node, bool isRoot, ITemplate template, IRenderFunction currentRenderFunction) {
 			if (node.NodeType != XmlNodeType.Element)
 				return false;
+			if (isRoot)
+				throw ParserUtils.TemplateErrorException("Control flow nodes cannot be root elements.");
+
+			if (Utils.NodeName(node) == "case" || Utils.NodeName(node) == "default")
+				throw ParserUtils.TemplateErrorException("<case> and <default> can only occur inside <switch>");
 
 			string statement = GetStatement(node);
 			if (statement == null)
 				return false;
-			
+
 			currentRenderFunction.AddFragment(new CodeFragment(statement + " {", 1));
 
-			Utils.DoForEachChild(node, delegate(XmlNode child) {
-				docProcessor.ProcessRecursive(child, template, currentRenderFunction);
-			});
+			if (Utils.NodeName(node) == "switch") {
+				ProcessSwitchContent(docProcessor, node, template, currentRenderFunction);
+			}
+			else {
+				Utils.DoForEachChild(node, delegate(XmlNode child) {
+					docProcessor.ProcessRecursive(child, template, currentRenderFunction);
+				});
+			}
 			
 			currentRenderFunction.AddFragment(new CodeFragment(null, -1));
 			currentRenderFunction.AddFragment(new CodeFragment("}", 0));
--- Saltarelle/Core/SaltarelleParser/SaltarelleParser.Tests/NodeProcessorTests/ControlFlowTagProcessorTests.cs
+++ Saltarelle/Core/SaltarelleParser/SaltarelleParser.Tests/NodeProcessorTests/ControlFlowTagProcessorTests.cs
@@ -31,6 +31,7 @@ namespace SaltarelleParser.Tests {
 			mocks.ReplayAll();
 			XmlNode n = Globals.GetXmlNode("<for each=\"int x in ints\"/>");
 			Assert.AreEqual("foreach (int x in ints)", ControlFlowTagProcessor.GetStatement(n));
+			mocks.VerifyAll();
 		}
 
 		[TestMethod]
@@ -38,6 +39,7 @@ namespace SaltarelleParser.Tests {
 			mocks.ReplayAll();
 			XmlNode n = Globals.GetXmlNode("<for stmt=\"int i = 0; i &lt; l.Length; i++\"/>");
 			Assert.AreEqual("for (int i = 0; i < l.Length; i++)", ControlFlowTagProcessor.GetStatement(n));
+			mocks.VerifyAll();
 		}
 
 		[TestMethod]
@@ -45,6 +47,7 @@ namespace SaltarelleParser.Tests {
 			mocks.ReplayAll();
 			XmlNode n = Globals.GetXmlNode("<for each=\"int x in ints\" stmt=\"int i = 0; i &lt; l.Length; i++\"/>");
 			Globals.AssertThrows(() => ControlFlowTagProcessor.GetStatement(n), (TemplateErrorException ex) => true);
+			mocks.VerifyAll();
 		}
 
 		[TestMethod]
@@ -52,6 +55,7 @@ namespace SaltarelleParser.Tests {
 			mocks.ReplayAll();
 			XmlNode n = Globals.GetXmlNode("<for/>");
 			Globals.AssertThrows(() => ControlFlowTagProcessor.GetStatement(n), (TemplateErrorException ex) => true);
+			mocks.VerifyAll();
 		}
 
 		[TestMethod]
@@ -59,6 +63,7 @@ namespace SaltarelleParser.Tests {
 			mocks.ReplayAll();
 			XmlNode n = Globals.GetXmlNode("<if test=\"x &gt; 0\"/>");
 			Assert.AreEqual("if (x > 0)", ControlFlowTagProcessor.GetStatement(n));
+			mocks.VerifyAll();
 		}
 
 		[TestMethod]
@@ -66,6 +71,7 @@ namespace SaltarelleParser.Tests {
 			mocks.ReplayAll();
 			XmlNode n = Globals.GetXmlNode("<if/>");
 			Globals.AssertThrows(() => ControlFlowTagProcessor.GetStatement(n), (TemplateErrorException ex) => true);
+			mocks.VerifyAll();
 		}
 
 		[TestMethod]
@@ -73,6 +79,7 @@ namespace SaltarelleParser.Tests {
 			mocks.ReplayAll();
 			XmlNode n = Globals.GetXmlNode("<while test=\"x &gt; 0\"/>");
 			Assert.AreEqual("while (x > 0)", ControlFlowTagProcessor.GetStatement(n));
+			mocks.VerifyAll();
 		}
 
 		[TestMethod]
@@ -80,10 +87,100 @@ namespace SaltarelleParser.Tests {
 			mocks.ReplayAll();
 			XmlNode n = Globals.GetXmlNode("<while/>");
 			Globals.AssertThrows(() => ControlFlowTagProcessor.GetStatement(n), (TemplateErrorException ex) => true);
+			mocks.VerifyAll();
+		}
+
+		[TestMethod]
+		public void TestGetStatement_SwitchWorks() {
+			mocks.ReplayAll();
+			XmlNode n = Globals.GetXmlNode("<switch expr=\"x\"/>");
+			Assert.AreEqual("switch (x)", ControlFlowTagProcessor.GetStatement(n));
+			mocks.VerifyAll();
+		}
+
+		[TestMethod]
+		public void TestGetStatement_SwitchThrowsIfNoExpression() {
+			mocks.ReplayAll();
+			XmlNode n = Globals.GetXmlNode("<switch/>");
+			Globals.AssertThrows(() => ControlFlowTagProcessor.GetStatement(n), (TemplateErrorException ex) => true);
+			mocks.VerifyAll();
 		}
 		
 		[TestMethod]
-		public void TestTryProcess_Works() {
+		public void TestProcessSwitchContents_ErrorIfNoContent() {
+			mocks.ReplayAll();
+			XmlNode node = Globals.GetXmlNode("<switch expr=\"testexpr\">  <!-- comment --> </switch>");
+			Globals.AssertThrows(() => ControlFlowTagProcessor.ProcessSwitchContent(docProcessor, node, template, renderFunction), (TemplateErrorException ex) => true);
+			mocks.VerifyAll();
+		}
+
+		[TestMethod]
+		public void TestProcessSwitchContents_ErrorIfBadElementContent() {
+			mocks.ReplayAll();
+			XmlNode node = Globals.GetXmlNode("<switch expr=\"testexpr\"><bad/><default>x</default></switch>");
+			Globals.AssertThrows(() => ControlFlowTagProcessor.ProcessSwitchContent(docProcessor, node, template, renderFunction), (TemplateErrorException ex) => true);
+			mocks.VerifyAll();
+		}
+
+		[TestMethod]
+		public void TestProcessSwitchContents_ErrorIfProcessingInstructionContent() {
+			mocks.ReplayAll();
+			XmlNode node = Globals.GetXmlNode("<switch expr=\"testexpr\"><?bad?><default>x</default></switch>");
+			Globals.AssertThrows(() => ControlFlowTagProcessor.ProcessSwitchContent(docProcessor, node, template, renderFunction), (TemplateErrorException ex) => true);
+			mocks.VerifyAll();
+		}
+
+		[TestMethod]
+		public void TestProcessSwitchContents_ErrorIfTwoDefaults() {
+			Expect.Call(() => docProcessor.ProcessRecursive(null, null, null)).IgnoreArguments().Repeat.Any();
+			mocks.ReplayAll();
+			XmlNode node = Globals.GetXmlNode("<switch expr=\"testexpr\"><default>x</default><default>y</default></switch>");
+			Globals.AssertThrows(() => ControlFlowTagProcessor.ProcessSwitchContent(docProcessor, node, template, renderFunction), (TemplateErrorException ex) => true);
+			mocks.VerifyAll();
+		}
+		
+		[TestMethod]
+		public void TestTryProcessSwitch_Works() {
+			XmlNode node = Globals.GetXmlNode("<switch expr=\"testexpr\"> <case value=\"1\"><x/></case>  <default><y/></default> <!-- comment --> <case value=\"2\"><z1/><z2/></case></switch>");
+			XmlNode x = node.SelectSingleNode("//x"), y = node.SelectSingleNode("//y"), z1 = node.SelectSingleNode("//z1"), z2 = node.SelectSingleNode("//z2");
+
+			Expect.Call(() => docProcessor.ProcessRecursive(x,  template, renderFunction)).Do((Action<XmlNode, ITemplate, IRenderFunction>)((_, __, f) => f.AddFragment(new LiteralFragment("[a]"))));
+			Expect.Call(() => docProcessor.ProcessRecursive(y,  template, renderFunction)).Do((Action<XmlNode, ITemplate, IRenderFunction>)((_, __, f) => f.AddFragment(new LiteralFragment("[b]"))));
+			Expect.Call(() => docProcessor.ProcessRecursive(z1, template, renderFunction)).Do((Action<XmlNode, ITemplate, IRenderFunction>)((_, __, f) => f.AddFragment(new LiteralFragment("[c1]"))));
+			Expect.Call(() => docProcessor.ProcessRecursive(z2, template, renderFunction)).Do((Action<XmlNode, ITemplate, IRenderFunction>)((_, __, f) => f.AddFragment(new LiteralFragment("[c2]"))));
+
+			mocks.ReplayAll();
+			Assert.IsTrue(new ControlFlowTagProcessor().TryProcess(docProcessor, node, false, template, renderFunction));
+			
+			Assert.IsTrue(new IFragment[] { new CodeFragment("switch (testexpr) {", 1),
+			                                new CodeFragment("case 1: {", 1),
+			                                new LiteralFragment("[a]"),
+			                                new CodeFragment("break;", -1),
+			                                new CodeFragment("}", 0),
+			                                new CodeFragment("default: {", 1),
+			                                new LiteralFragment("[b]"),
+			                                new CodeFragment("break;", -1),
+			                                new CodeFragment("}", 0),
+			                                new CodeFragment("case 2: {", 1),
+			                                new LiteralFragment("[c1]"),
+			                                new LiteralFragment("[c2]"),
+			                                new CodeFragment("break;", -1),
+			                                new CodeFragment("}", 0),
+			                                new CodeFragment(null, -1),
+			                                new CodeFragment("}", 0) }.SequenceEqual(fragments));
+
+			mocks.VerifyAll();			
+		}
+
+		[TestMethod]
+		public void TestTryProcess_ErrorIfRoot() {
+			mocks.ReplayAll();
+			Globals.AssertThrows(() => new ControlFlowTagProcessor().TryProcess(docProcessor, Globals.GetXmlNode("<if test=\"x\"/>"), true, template, renderFunction), (TemplateErrorException ex) => true);
+			mocks.VerifyAll();
+		}
+		
+		[TestMethod]
+		public void TestTryProcess_NonSwitchWorks() {
 			XmlNode n = Globals.GetXmlNode("<if test=\"x\"><a/><b/></if>"), n1 = n.ChildNodes[0], n2 = n.ChildNodes[1];
 			Expect.Call(() => docProcessor.ProcessRecursive(n1, template, renderFunction)).Do((Action<XmlNode, ITemplate, IRenderFunction>)((_, __, f) => f.AddFragment(new LiteralFragment("a"))));
 			Expect.Call(() => docProcessor.ProcessRecursive(n2, template, renderFunction)).Do((Action<XmlNode, ITemplate, IRenderFunction>)((_, __, f) => f.AddFragment(new LiteralFragment("b"))));
