5360db495828e8471478e2155c13f54276e3cb2e
erik.kallen@gmail.com <erik.kallen@gmail.com@7e296dba-0ccf-11df-bbbe-ede73d0abfb9>
2010-11-24 15:53:30 +0000

    Added utility methods to ControlDocumentFragment
    git-svn-id: http://saltarelle.googlecode.com/svn/trunk@128 7e296dba-0ccf-11df-bbbe-ede73d0abfb9

--- Saltarelle/Core/SaltarelleLib/SaltarelleLib.Shared/ControlDocumentFragment.Shared.cs
+++ Saltarelle/Core/SaltarelleLib/SaltarelleLib.Shared/ControlDocumentFragment.Shared.cs
@@ -13,16 +13,17 @@ namespace Saltarelle {
 		public string[] scriptReferences;
 		public string[] startupScripts;
 		public string controlType;
-		public string id;
 		public string html;
+		public object configObject;
 		
 #if SERVER
 		public ControlDocumentFragment(IControl control) {
 			var sm = GlobalServices.Provider.GetService<IScriptManagerService>();
-			this.id = control.Id = Guid.NewGuid().ToString().Replace("-", "");
+			control.Id = Guid.NewGuid().ToString().Replace("-", "");
 			this.scriptReferences = sm.GetAllRequiredIncludes().ToArray();
 			this.startupScripts = sm.GetStartupScripts().ToArray();
 			this.html = control.Html;
+			this.configObject = control.ConfigObject;
 			this.controlType = control.GetType().FullName;
 		}
 #endif
@@ -30,16 +31,19 @@ namespace Saltarelle {
 
 #if CLIENT
 	public static class DocumentFragmentHelper {
-		public static IControl Inject(ControlDocumentFragment f, string newId, DOMElement parent) {
+		public static void PrepareForInject(ControlDocumentFragment f) {
 			for (int i = 0; i < f.scriptReferences.Length; i++)
 				((IScriptManagerService)GlobalServices.Provider.GetService(typeof(IScriptManagerService))).EnsureScriptIncluded(f.scriptReferences[i]);
-
-			JQueryProxy.jQuery(parent).html(f.html);
 			foreach (string s in f.startupScripts)
 				Script.Eval(s);
+		}
+	
+		public static IControl Inject(ControlDocumentFragment f, string newId, DOMElement parent) {
+			PrepareForInject(f);
+			JQueryProxy.jQuery(parent).html(f.html);
 
 			Type tp = Utils.FindType(f.controlType);
-			IControl control = (IControl)Type.CreateInstance(tp, f.id);
+			IControl control = (IControl)Type.CreateInstance(tp, f.configObject);
 			control.Id = newId;
 			
 			return control;
