5d46504e9d7285adf58878206deaa182ab810298
erik.kallen@gmail.com <erik.kallen@gmail.com@7e296dba-0ccf-11df-bbbe-ede73d0abfb9>
2011-02-14 14:17:25 +0000

    Updated version to 0.7.2
    git-svn-id: http://saltarelle.googlecode.com/svn/trunk@152 7e296dba-0ccf-11df-bbbe-ede73d0abfb9

--- Saltarelle/SaltarelleVersion.cs
+++ Saltarelle/SaltarelleVersion.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.7.1")]
-[assembly: AssemblyFileVersion("0.7.1")]
+[assembly: AssemblyVersion("0.7.2")]
+[assembly: AssemblyFileVersion("0.7.2")]
