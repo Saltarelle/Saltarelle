e6bd87818611501cde4c062aed6b2e05724be650
erikkallen <erik_kallen@hotmail.com>
2012-01-26 15:57:23 +0100

    Some more installer fixes

--- Saltarelle/NuGet/InstallCore.ps1
+++ Saltarelle/NuGet/InstallCore.ps1
@@ -8,7 +8,7 @@ Function MakeRelativePath($Origin, $Target) {
     $originUri.MakeRelativeUri($targetUri).ToString().Replace('/', [System.IO.Path]::DirectorySeparatorChar)
 }
 
-Function AddEmbeddedResource([string]$RelativePath, [switch]$DeleteFileIfAdded) {
+Function AddFile([string]$RelativePath, [string]$ItemType, [switch]$DeleteFileIfAdded) {
 	$fileName = [System.IO.Path]::GetFileName($RelativePath)
 	if (-not ($project.ProjectItems | ? { $_.Name -eq $fileName })) {
 		$filePath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($project.FileName), $RelativePath))
@@ -17,7 +17,7 @@ Function AddEmbeddedResource([string]$RelativePath, [switch]$DeleteFileIfAdded) 
 			$added = $true
 		}
 		$item = $project.ProjectItems.AddFromFile($filePath)
-		$item.Properties.Item("ItemType").Value = "EmbeddedResource"
+		$item.Properties.Item("ItemType").Value = $ItemType
 
 		if ($added -and $DeleteFileIfAdded) {
 			rm $filePath > $null
@@ -25,12 +25,20 @@ Function AddEmbeddedResource([string]$RelativePath, [switch]$DeleteFileIfAdded) 
 	}
 }
 
+Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+$rootNamespace = $msbuild.Properties | ? { $_.Name -eq "RootNamespace" } | % { $_.UnevaluatedValue } | Select-Object -First 1
+if ($rootNamespace -eq $project.Name) {
+	$msbuild.SetProperty("RootNamespace", ($project.Name -replace "\.(Client|Server)$",""))
+}
+
 if ($isClient) {
 	$project.Object.References.Item("SaltarelleLib").Remove()
 	$project.Object.References.Item("Newtonsoft.Json").Remove()
 
-    Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
-    $msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+	$project.Object.References | ? { $_.Name.StartsWith("System.") } | % { $_.Remove() }
+	$project.Object.References | ? { $_.Name -eq "System" } | % { $_.Remove() }
+	$project.Object.References | ? { $_.Name.StartsWith("Microsoft.") } | % { $_.Remove() }
 
 	$msbuild.Xml.Imports | ? { $_.Project.EndsWith("nStuff.ScriptSharp.targets") } | % { $msbuild.Xml.RemoveChild($_) }
 	$msbuild.Xml.Imports | ? { $_.Project.EndsWith("Microsoft.CSharp.targets") } | % { $msbuild.Xml.RemoveChild($_) }
@@ -38,13 +46,24 @@ if ($isClient) {
 	
 	$msbuild.Xml.Imports | ? { $_.Project.EndsWith("Saltarelle.targets") } | % { $msbuild.Xml.RemoveChild($_) }
 	$msbuild.Xml.AddImport("`$(SolutionDir)$(MakeRelativePath -Origin $project.DTE.Solution.FullName -Target ([System.IO.Path]::Combine($toolsPath, ""Saltarelle.targets"")))")
+	
+	$msbuild.SetProperty("NoStdLib", "True")
+	$msbuild.SetProperty("TemplateFile", "Properties\Script.jst")
+	
+	$propertiesDir = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($project.FileName), "Properties")
+	
+	if (-not (Test-Path $propertiesDir)) {
+		md $propertiesDir
+	}
+	"#include[as-is] ""%code%""" | Out-File ([System.IO.Path]::Combine($propertiesDir, "Script.jst")) -Encoding UTF8
+	AddFile "Properties\Script.jst" -ItemType "Content"
 }
 else {
 	$project.Object.References.Item("SaltarelleLib.Client").Remove()
 	$project.Object.References.Item("sscorlib").Remove()
 
-	AddEmbeddedResource "..\Client.dll" -DeleteFileIfAdded
-	AddEmbeddedResource "..\Script.js" -DeleteFileIfAdded
-	AddEmbeddedResource "..\Script.min.js" -DeleteFileIfAdded
-	AddEmbeddedResource "Module.less"
+	AddFile "..\Client.dll" -ItemType "EmbeddedResource" -DeleteFileIfAdded
+	AddFile "..\Script.js" -ItemType "EmbeddedResource" -DeleteFileIfAdded
+	AddFile "..\Script.min.js" -ItemType "EmbeddedResource" -DeleteFileIfAdded
+	AddFile "Module.less" -ItemType "EmbeddedResource"
 }
