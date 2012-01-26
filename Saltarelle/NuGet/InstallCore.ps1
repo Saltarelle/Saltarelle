10f608ae55b86c5b7cb1e37a3e702dd7065dd639
erikkallen <erik_kallen@hotmail.com>
2012-01-26 18:26:11 +0100

    Add an ordering dependency from the server project to the client project.

--- Saltarelle/NuGet/InstallCore.ps1
+++ Saltarelle/NuGet/InstallCore.ps1
@@ -1,6 +1,10 @@
 param($installPath, $toolsPath, $package, $project)
 
+Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+
 $isClient = $project.Name.EndsWith(".Client")
+$canonicalName = ($project.Name -replace "\.(Client|Server)$","")
 
 Function MakeRelativePath($Origin, $Target) {
     $originUri = New-Object Uri('file://' + $Origin)
@@ -37,10 +41,15 @@ Function Add-DefineConstant($Configuration, [string]$Constant) {
 	}
 }
 
-$canonicalName = ($project.Name -replace "\.(Client|Server)$","")
-
-Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
-$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+Function Add-OrderingDependency($From, $To) {
+	If (-not ($From.Object.References | ? { $_.SourceProject -eq $To })) {
+		$msb = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($From.FullName) | Select-Object -First 1
+		$relPath = MakeRelativePath -Origin $From.FullName -Target $To.FullName
+		$ref = $msb.AddItem("ProjectReference", $relPath) | Select-Object -First 1
+		$ref.SetMetadataValue("ReferenceOutputAssembly", "false") > $null
+		$ref.SetMetadataValue("Name", "$($To.Name) (ordering dependency only)") > $null
+	}
+}
 
 # Exclude trailing .Client/.Server from the root namespace (unless it has already been changed)
 $rootNamespace = $msbuild.Properties | ? { $_.Name -eq "RootNamespace" } | % { $_.UnevaluatedValue } | Select-Object -First 1
@@ -81,6 +90,12 @@ if ($isClient) {
 	
 	# Add the CLIENT define constant
 	$project.ConfigurationManager | % { Add-DefineConstant -Configuration $_ -Constant "CLIENT" }
+	
+	# Add a reference from the server project to this project
+	$serverProject = $project.Collection | ? { $_.Name -eq "$canonicalName.Server" }
+	if ($serverProject) {
+		Add-OrderingDependency -From $serverProject -To $project
+	}
 }
 else {
 	# Remove serverside references added by us
@@ -100,5 +115,11 @@ else {
 	$assemblyName = $msbuild.Properties | ? { $_.Name -eq "AssemblyName" } | % { $_.UnevaluatedValue } | Select-Object -First 1
 	if ($assemblyName -eq $project.Name) {
 		$msbuild.SetProperty("AssemblyName", $canonicalName)
+	}
+	
+	# Add a reference from this project to the corresponding client project
+	$clientProject = $project.Collection | ? { $_.Name -eq "$canonicalName.Client" }
+	if ($clientProject) {
+		Add-OrderingDependency -From $project -To $clientProject
 	}
 }
